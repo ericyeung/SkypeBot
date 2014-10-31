b81d0b280f0f5b580d66178d684e72497e1f71a7
Ryan Ly <windask@hotmail.com>
2014-10-31 14:32:52 -0400

    Improved flexibility

--- skype.py
+++ skype.py
@@ -1,21 +1,26 @@
 import Skype4Py
 import threading
 
-def print_checkin():
-	for elem in skypeClient.BookmarkedChats:  # Looks in bookmarked chats and returns True if chat is found.
-		if len(elem.Members) > 2 and (member.Handler == "irlightbrite" for member in elem.Members): #Make Sure Clarence is in the chat
-			print "Checking in."
-			elem.SendMessage("#checkin")
-
+def print_checkin(participants):
+    for elem in skypeClient.BookmarkedChats:  # Looks in bookmarked chats and returns True if chat is found.
+        for member in elem._GetActiveMembers():
+            try:
+                participants.remove(member._GetFullName())
+            except:
+                pass
+        if not participants:
+            print "Checking in."
+            elem.SendMessage("#checkin")
 
 class TaskThread(threading.Thread):
     """Thread that executes a task every N seconds"""
     
-    def __init__(self, task):
+    def __init__(self, task, args):
         threading.Thread.__init__(self)
         self._finished = threading.Event()
         self._interval = 3600
         self.task = task
+        self.args = args
     
     def setInterval(self, interval):
         """Set the number of seconds we sleep between executing our task"""
@@ -24,14 +29,16 @@ class TaskThread(threading.Thread):
     def run(self):
         while 1:
             if self._finished.isSet(): return
-            self.task()
-
+            self.task(self.args)
             self._finished.wait(self._interval)
 
+# Type in members of the groups you want to checkin with. 
+#Eg. members = ['John Doe'] will #checkin to all favourited groups who have John Doe
+members = []
 
 # Create an instance of the Skype class.
 skypeClient = Skype4Py.Skype()
 skypeClient.Attach()
 
-task =TaskThread(print_checkin)
+task =TaskThread(print_checkin, members)
 task.run()
