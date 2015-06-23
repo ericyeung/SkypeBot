c4dfa5ceffe89735eef09e386e33f5a354e0b458
ericyeung <ericyeung@users.noreply.github.com>
2015-06-23 13:31:18 -0400

    Added some new commands

--- skype.py
+++ skype.py
@@ -22,6 +22,8 @@ def print_checkin(participants):
             elem.SendMessage("#checkin")
             elem.SendMessage("I am a bot")
 
+            # Do not want more than one bot spamming streamers
+            """   
             for streamer in streamerList:
                 h = httplib2.Http(".cache")
                 resp, content = h.request(streamerList[streamer], "GET")
@@ -30,12 +32,29 @@ def print_checkin(participants):
                 if (data['stream']):
                     print streamer + "'s stream is up!"
                     elem.SendMessage(streamer + "'s stream is up! - http://www.twitch.tv/" + streamer)
+            """
 
+from pyteaser import SummarizeUrl
+            
 def commands(Message, Status):
     if Status == 'SENT' or (Status == 'RECEIVED'):
+        body = Message.Body
         if Message.Body == "#test":
             cmd_test(Message)
         
+        elif Message.Body == "#poll":
+            cmd_poll(Message)
+
+        elif Message.Body == "#help":
+            cmd_help(Message)
+        
+        elif body.startswith("#tldr"):            
+            parts = body.split() #splits the message into command and arg
+            command = parts[0][1:]  # this is the #tldr part
+            url = commands[command](*parts[1:])  # applys the command with our argument (e.g. the article summarized)
+            summaries = SummarizeUrl(url)  
+            Message.Chat.SendMessage(summaries)
+            
         else:
             pass
     else:
@@ -49,6 +68,19 @@ def cmd_test(Message):
     Message.Chat.SendMessage('Robot: Testing3')
     time.sleep(1.)
     print "Testing complete.\n"
+
+def cmd_help(Message):
+    Message.Chat.SendMessage('#test for beep boops')
+    Message.Chat.SendMessage('#poll for arena help')
+
+import random as rand
+arena_cards = ["first card","second card","third card"]
+
+def getpollanswer():
+    return arena_cards[rand.randint(0,2)]
+
+def cmd_poll(Message):
+    Message.Chat.SendMessage(getpollanswer() + ".")
 
 
 class TaskThread(threading.Thread):
