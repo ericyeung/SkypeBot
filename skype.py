ed52e98afb027551b3467cd79dec663e84c26a8f
Eric Yeung <irq.ericyeung@hotmail.com>
2015-07-26 12:31:09 -0400

    Making it more modular

--- skype.py
+++ skype.py
@@ -1,8 +1,9 @@
-# -*- coding: utf-8 -*-
+	# -*- coding: utf-8 -*-
 import Skype4Py
 import threading
 import httplib2, json
 import os, time
+from tldr import cmd_tldr
 
 def print_checkin(participants):
     for elem in skypeClient.BookmarkedChats:  # Looks in bookmarked chats and returns True if chat is found.
@@ -18,10 +19,6 @@ def print_checkin(participants):
             elem.SendMessage("#checkin")
             elem.SendMessage(">> I am BiscuitsBot")
 
-from pyteaser import SummarizeUrl
-from goose import Goose
-sentences_per_summary = 3
- 
 def commands(Message, Status):
     if Status == 'SENT' or (Status == 'RECEIVED'):
         msg = Message.Body.lower()
@@ -36,20 +33,7 @@ def commands(Message, Status):
             cmd_help(Message)
         
         elif body.startswith("#tldr"):            
-            Message.Chat.SendMessage(">> Summarizing your article...")
-            splitMessage = body.strip().split(" ") # splits the message into command and argument
-            url = splitMessage[1] 
-            summaries = SummarizeUrl(url)  
-            g = Goose()
-
-            if summaries:
-                for summary in summaries:
-                    Message.Chat.SendMessage(summary.encode("utf-8"))
-                g = Goose()
-                article = g.extract(url)
-                Message.Chat.SendMessage(">> The article title is" + " \""+ article.title + "\"")
-            else:
-                Message.Chat.SendMessage(">> The article is too powerful... Cannot summarize.")
+        	cmd_tldr()
 
         elif body.startswith("#stopbot"):
             Message.Chat.SendMessage('>> Sleeping... for a while')
@@ -104,7 +88,6 @@ def cmd_poll(Message):
     Message.Chat.SendMessage(">> choose the" + getpollanswer())
 
 class TaskThread(threading.Thread):
-
 
     """Thread that executes a task every N seconds"""
     
