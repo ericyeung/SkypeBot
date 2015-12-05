a04d37bd7ac45301a493f20c038bf379196c7743
Eric <ericyeung@users.noreply.github.com>
2015-12-04 22:14:08 -0500

    Added video emoji detection

--- skype.py
+++ skype.py
@@ -4,10 +4,10 @@ import threading
 import httplib2, json
 import os, time
 from tldr import cmd_tldr
-
+ 
 def print_checkin(participants):
     global botstatus
-
+ 
     for elem in skypeClient.BookmarkedChats:  # Looks in bookmarked chats and returns True if chat is found.
         participantsList = list(participants)
         for member in elem._GetActiveMembers():
@@ -20,69 +20,72 @@ def print_checkin(participants):
             print (time.strftime("%H:%M:%S")) # timestamps for checkins
             #elem.SendMessage("#checkin")
             #elem.SendMessage(">> I am BiscuitsBot")
-
+ 
 botstatus = True
-
+ 
 def commands(Message, Status):
     global botstatus
-
+ 
     if Status == 'SENT' or (Status == 'RECEIVED'):
         msg = Message.Body.lower()
         body = Message.Body
         if body == "#beep":
             cmd_test(Message)
-        
+       
         elif body == "#poll":
             cmd_poll(Message)
-
+ 
         elif body == "#help":
             cmd_help(Message)
-        
+       
         elif body.startswith("#tldr"):            
-        	cmd_tldr(Message)
-
+                cmd_tldr(Message)
+ 
         elif body.startswith("#sleep"):
             splitMessage = body.strip().split(" ") # splits the message into command and argument
             sleeptime = splitMessage[1]
             sleepint = int(float(sleeptime))
             Message.Chat.SendMessage('>> Sleeping... for' + sleepint + 'minutes')
-            time.sleep(60.*sleepint) # 30 = 30 minutes 
+            time.sleep(60.*sleepint) # 30 = 30 minutes
             print "Script paused..."
             Message.Chat.SendMessage('>> BiscuitsBot is awake!')
             print "Script resumed."
-
+ 
         elif body.startswith("#killbot"):
             splitMessage = body.strip().split(" ") # splits the message into command and argument
             secretpassword = splitMessage[1]
             if secretpassword == ("kappa123"):
                 Message.Chat.SendMessage('>> Correct password, goodbye!')
                 os._exit(0)
-            else: 
+            else:
                 Message.Chat.SendMessage('>> WRONG PASSWORD, UNABLE TO TERMINATE')
-
+ 
         elif body.startswith("#call"):
             #GetCallWith()
             Message.Chat.SendMessage('>> Sorry! This feature is not yet available.')
-
+ 
         elif body.startswith("#checkin") and Status == "RECEIVED":
-            Message.Chat.SendMessage(">> Thanks for checking in, " + Message.Sender.Handle + "!") 
-
+            Message.Chat.SendMessage(">> Thanks for checking in, " + Message.Sender.Handle + "!")
+ 
         elif body.startswith("#checkout"):
             Message.Chat.SendMessage(">> Bye " + Message.Sender.Handle + "!")
-
+ 
         elif body.startswith("#test1"):
-            botstatus = False 
+            botstatus = False
             Message.Chat.SendMessage(" >> test1")
-        
+       
         elif body.startswith("#test2"):
             botstatus = True
             Message.Chat.SendMessage(" >> test2")
-
+ 
+        elif body.startswith("Moji"):
+            Message.Chat.SendMessage("EMOJI DETECTED")
+ 
         else:
             pass
     else:
         pass
-
+ 
 def cmd_test(Message):
     Message.Chat.SendMessage('>> Beep')
     time.sleep(.5)
@@ -91,7 +94,7 @@ def cmd_test(Message):
     Message.Chat.SendMessage('>> Beep')
     time.sleep(.5)
     print "Testing complete.\n"
-
+ 
 def cmd_help(Message):
     Message.Chat.SendMessage('>> #beep for beep boops')
     Message.Chat.SendMessage('>> #poll for arena help')
@@ -99,50 +102,49 @@ def cmd_help(Message):
     Message.Chat.SendMessage('>> #stopbot to put BiscuitsBot to sleep')
     Message.Chat.SendMessage('>> #call for group calls [do not use this yet]')
     Message.Chat.SendMessage('>> #killbot to murder BiscuitsBot')
-
+ 
 import random as rand
 arena_cards = ["first card","second card","third card"]
-
+ 
 def getpollanswer():
     return arena_cards[rand.randint(0,2)]
-
+ 
 def cmd_poll(Message):
     Message.Chat.SendMessage(">> choose the " + getpollanswer())
-
+ 
 class TaskThread(threading.Thread):
-
+ 
     """Thread that executes a task every N seconds"""
-    
+   
     def __init__(self, task, args):
         threading.Thread.__init__(self)
         self._finished = threading.Event()
         self._interval = 3600
         self.task = task
         self.args = args
-    
+   
     def setInterval(self, interval):
         """Set the number of seconds we sleep between executing our task"""
         self._interval = interval
-    
+   
     def run(self):
         while 1:
             if self._finished.isSet(): return
             self.task(self.args)
             self._finished.wait(self._interval)
-
-# Type in members of the groups you want to checkin with. 
+ 
+# Type in members of the groups you want to checkin with.
 #Eg. members = ['John Doe'] will #checkin to all favourited groups who have John Doe
 members = []
-
+ 
 # Create an instance of the Skype class.
 skypeClient = Skype4Py.Skype()
 skypeClient.Attach()
-
-skypeClient.OnMessageStatus = commands 
-
+ 
+skypeClient.OnMessageStatus = commands
+ 
 task =TaskThread(print_checkin, members)
 task.run()
-
+ 
 if __name__ == "__main__":
     main()
-            
