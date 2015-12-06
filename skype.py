f2efd1c9914e275f4fc69d2bb245dd95c92e205f
Eric <ericyeung@users.noreply.github.com>
2015-12-06 01:14:04 -0500

    Update skype.py

--- skype.py
+++ skype.py
@@ -130,35 +130,35 @@ def commands(Message, Status):
                     Message.Chat.SendMessage("You now have a " + item + "!") 
                 
                 elif (item == "Terrible_Shotgun"):
-                    weapon[MSH] += 5
+                    weapon[MSH] += 5 # Dual Wield
                     Message.Chat.SendMessage("You now have a " + item + "(5 attack)!")    
               
                 elif (item == "MIRV"):
-                    weapon[MSH] += 10 
+                    weapon[MSH] = 10 
                     Message.Chat.SendMessage("You now have a " + item + "(10 attack)!")                         
 
                 elif (item == "Black_People_Pesticide"):
-                    weapon[MSH] += 20 
+                    weapon[MSH] = 20 
                     Message.Chat.SendMessage("You now have a " + item + "(20 attack)!")     
 
                 elif (item == "Vault101"):
-                    armour[MSH] += 3
+                    armour[MSH] = 3
                     Message.Chat.SendMessage("You now have a " + item + "(3 armour)!")     
 
                 elif (item == "Leather"):
-                    armour[MSH] += 6
+                    armour[MSH] = 6
                     Message.Chat.SendMessage("You now have a " + item + "(6 armour)!")     
 
                 elif (item == "Metal"):
-                    armour[MSH] += 9
+                    armour[MSH] = 9
                     Message.Chat.SendMessage("You now have a " + item + "(9 armour)!")     
 
                 elif (item == "Combat"):
-                    armour[MSH] += 12
+                    armour[MSH] = 12
                     Message.Chat.SendMessage("You now have a " + item + "(12 armour)!")     
 
                 elif (item == "Power"):
-                    armour[MSH] += 15
+                    armour[MSH] = 15
                     Message.Chat.SendMessage("You now have a " + item + "(15 armour)!")     
         
         elif body.startswith("#heal"):
