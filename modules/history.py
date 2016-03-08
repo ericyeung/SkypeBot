4b22e217609c2ea62b64b276a72094ed6238fe09
Ryan Ly <windask@hotmail.com>
2016-03-08 18:13:39 -0500

    Filter commands from history

--- modules/history.py
+++ modules/history.py
@@ -4,6 +4,9 @@ from boto3.dynamodb.conditions import Key
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('log_messages')
 
+def filter_commands(results):
+    return filter(lambda x: not x['message'].startswith("%"), results)
+
 def get_log_messages(chat, num):
     try:
         limit = 10
@@ -15,7 +18,7 @@ def get_log_messages(chat, num):
         result = table.query(KeyConditionExpression=Key('chat').eq(chat),
                              ScanIndexForward=False,
                              Limit=min(10, limit))['Items']
-        return True, list(reversed(result))
+        return True, list(reversed(filter_commands(result)))
     except:
         return False, "Error"
 
@@ -37,10 +40,10 @@ def query_log_messages_frequency(chat, query):
 
     try:
         q_result = table.query(ScanIndexForward=False,
-                             KeyConditions={'chat': { 'AttributeValueList': [ chat ], 'ComparisonOperator': 'EQ' }},
-                             QueryFilter={'message': { 'AttributeValueList': [ query ], 'ComparisonOperator': 'CONTAINS' }})
+                               KeyConditions={'chat': { 'AttributeValueList': [ chat ], 'ComparisonOperator': 'EQ' }},
+                               QueryFilter={'message': { 'AttributeValueList': [ query ], 'ComparisonOperator': 'CONTAINS'}})
         result = q_result['Items']
         last_key = q_result.get('LastEvaluatedKey', None)
-        return True, list(reversed(result)), last_key
+        return True, list(reversed(filter_commands(result))), last_key
     except:
         return False, "Error", None
